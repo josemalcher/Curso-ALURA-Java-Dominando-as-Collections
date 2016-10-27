0fab25d0cbe4510eebf68d164b1cac6f830c86b1
José Malcher Jr <malcher.malch@gmail.com>
2016-10-27 17:00:34 -0300

    04 Mais práticas com relacionamentos - Ajuste e melhorias nas listas - unmodifiable list, Collections e ordenações

--- Gerenciador-de-Cursos/src/alura/Curso.java
+++ Gerenciador-de-Cursos/src/alura/Curso.java
@@ -49,4 +49,14 @@ public class Curso {
 		this.aulas.add(aula);
 	}
 
+	public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+
+	@Override
+	public String toString() {
+		return "[Curso: " + this.getNome() + ", tempo total: " + this.getTempoTotal() + ", aulas: + " + this.aulas
+				+ "]";
+	}
+
 }
