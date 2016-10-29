a3c66bfe53aa86712d84489301988be201f0b05e
José Malcher Jr <malcher.malch@gmail.com>
2016-10-29 19:37:38 -0300

    10 Mapas

---
+++ Gerenciador-de-Cursos/src/alura/TestaBuscaAlunosNoCurso.java
@@ -0,0 +1,23 @@
+package alura;
+
+public class TestaBuscaAlunosNoCurso {
+	public static void main(String[] args) {
+		Curso javaColecoes = new Curso("Dominando as colecoes do Java", "Paulo Silveira");
+
+		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando com colecoes", 24));
+
+		Aluno a1 = new Aluno("Jose Malcher", 12345);
+		Aluno a2 = new Aluno("Maria Acildes", 02222);
+		Aluno a3 = new Aluno("Luciana Souza", 44444);
+
+		javaColecoes.matricula(a1);
+		javaColecoes.matricula(a2);
+		javaColecoes.matricula(a3);
+		
+		System.out.println("Quem é o aluno matriculado 44444");
+		Aluno aluno = javaColecoes.buscaMatriculado(41444);
+		System.out.println("Aluno: "+aluno);
+	}
+}
