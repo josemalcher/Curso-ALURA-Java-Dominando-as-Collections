08f391ae94825f83457633b9f3a7a6899e86c6cf
José Malcher Jr <malcher.malch@gmail.com>
2016-10-29 18:08:45 -0300

    09 QUal collection Usar

---
+++ Gerenciador-de-Cursos/src/alura/QualColecaoUsar.java
@@ -0,0 +1,16 @@
+package alura;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class QualColecaoUsar {
+
+	public static void main(String[] args) {
+		
+		Collection<Aluno> alunos = new HashSet<>();
+		System.out.println(alunos.size());
+		//alunos.get(2);
+
+	}
+
+}
